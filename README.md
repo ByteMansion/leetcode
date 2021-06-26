def77e766b44f7f0db88f106fba67d5e369f29ad
Changrui <scrdyx@163.com>
2021-06-26 21:59:46 +0800

    [Leetcode 122] add solution to leetcode 122

--- README.md
+++ README.md
@@ -73,6 +73,7 @@ $ g++ -std=c++17 array/leetcode53.cpp -o leetcode53
 |112|[Leetcode 112 Path Sum](https://leetcode-cn.com/problems/path-sum/)|[solution](./tree/leetcode112.cpp)|postorder traversal|
 |120|[Leetcode 120 Triangle](https://leetcode-cn.com/problems/triangle/)|[solution](./array/leetcode120.cpp)|dynamic programming|
 |121|[Leetcode 121 Best Time to Buy and Sell Stock](https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/)|[solution](./array/leetcode121.cpp)||
+|122|[Leetcode 122 Best Time to Buy and Sell Stock](https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/)|[solution](./array/leetcode122.cpp)|greedy method|
 |135|[Leetcode 135 Candy](https://leetcode-cn.com/problems/candy/)|[solution](./array/leetcode135.cpp)|greedy algorithm|
 |144|[Leetcode 144 Binary Tree Preorder Traversal](https://leetcode-cn.com/problems/binary-tree-preorder-traversal/)|[solution](./tree/leetcode144.cpp)|**preorder traversal**|
 |l45|[Leetcode 145 Binary Tree Postorder Traversal](https://leetcode-cn.com/problems/binary-tree-postorder-traversal/)|[solution](./tree/leetcode145.cpp)|**postorder traversal**|
---
+++ array/leetcode122.cpp
@@ -0,0 +1,68 @@
+/**
+ * @brief   Leetcode 122 Best Time to Buy and Sell Stock II
+ * You are given an array prices where prices[i] is the price of a 
+ * given stock on the ith day.
+ * 
+ * Find the maximum profit you can achieve. You may complete as many 
+ * transactions as you like (i.e., buy one and sell one share of the 
+ * stock multiple times).
+ * 
+ * Note: You may not engage in multiple transactions simultaneously 
+ * (i.e., you must sell the stock before you buy again).
+ */
+#include "../include/utils.hpp"
+#include <vector>
+
+class Solution
+{
+public:
+    /**
+     * @brief   Greedy Method
+     * 
+     * @param prices 
+     * @return int 
+     */
+    int maxProfit(vector<int> &prices)
+    {
+        int n = prices.size();
+        if (n == 1) {
+            return 0;
+        }
+        int lIdx = 0, rIdx = 0, total = 0;
+        for (int i = 1; i < n; i++)
+        {
+            if (prices[i] <= prices[i - 1]) {
+                if (rIdx != lIdx) {
+                    total += prices[rIdx] - prices[lIdx];
+                }
+                lIdx = i;
+            }
+            rIdx = i;
+        }
+        if (rIdx != lIdx) {
+            total += prices[rIdx] - prices[lIdx];
+        }
+        return total;
+    }
+};
+
+int main()
+{
+    vector<int> prices;
+    Solution obj;
+    int result;
+
+    // case 1
+    prices = {7, 1, 5, 3, 6, 4};
+    print_array(prices);
+    result = obj.maxProfit(prices);
+    cout << result << endl;
+
+    // case 2
+    prices = {7, 6, 5, 3, 2, 1};
+    print_array(prices);
+    result = obj.maxProfit(prices);
+    cout << result << endl;
+
+    return 0;
+}
