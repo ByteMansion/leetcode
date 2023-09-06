527bb79cd9f365dbf927f9f0217d4b7a780db584
changrui.shao <changrui.shao@longsys.com>
2023-09-06 09:04:23 +0800

    [Leetcode 127] add new solution using bidirectional BFS to leetcode 127

--- string/leetcode127.cpp
+++ string/leetcode127.cpp
@@ -10,27 +10,27 @@
 class Solution {
 public:
     /**
-     * Solution: Breadth-first traversal
-     * @brief In this problem, breadth-first traversal should be used, but there is no
+     * Solution 1: Breadth-First Search
+     * @brief In this problem, breadth-first search should be used, but there is no
      *        graph or tree structure. Therefore, the step should be as follows:
      * 1. create graph;
-     * 2. breadth-first traversal
+     * 2. breadth-first search
      */
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
         // 1. construct hash set
-        unordered_set<string> hashSet;
+        unordered_set<string> wordSet;
         for (auto& str: wordList) {
-            hashSet.insert(str);
+            wordSet.insert(str);
         }
-        if (hashSet.find(endWord) == hashSet.end()) {
+        if (wordSet.find(endWord) == wordSet.end()) {
             return 0;
         }
         // 2. BFS
         int result = 1;
         queue<string> level;
-        unordered_set<string> visited;
         level.push(beginWord);
-        visited.insert(beginWord);
+        unordered_set<string> visitedSet;
+        visitedSet.insert(beginWord);
         queue<string> tmpLevel;
         while (!level.empty()) {
             string word = level.front();
@@ -38,8 +38,8 @@ public:
             if (word == endWord) {
                 return result;
             }
-            visited.insert(word);
-            oneLetterDistance(word, hashSet, tmpLevel, visited);
+            visitedSet.insert(word);
+            oneLetterDistance(word, wordSet, tmpLevel, visitedSet);
             
             if (level.empty()) {
                 result += 1;
@@ -49,8 +49,71 @@ public:
         }
         return 0;
     }
+
+    /**
+     * Solution 2: Bidirectional BFS
+     * 
+     */
+    int ladderLength2(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> wordSet;
+        for (auto& str: wordList) {
+            wordSet.insert(str);
+        }
+        if (wordSet.find(endWord) == wordSet.end()) {
+            return 0;
+        }
+
+        int result = 1;
+        unordered_set<string> beginLevel;
+        beginLevel.insert(beginWord);
+        unordered_set<string> endLevel;
+        endLevel.insert(endWord);
+        unordered_set<string> visitedSet;
+        unordered_set<string> tmpLevel, swapLevel;
+        while (!beginLevel.empty() && !endLevel.empty()) {
+            if (beginLevel.size() > endLevel.size()) {
+                swapLevel = beginLevel;
+                beginLevel = endLevel;
+                endLevel = swapLevel;
+            }
+            for (auto word: beginLevel) {
+                visitedSet.insert(word);
+                if (oneLetterDistance(word, wordSet, tmpLevel, visitedSet, endLevel)) {
+                    return result + 1;
+                }
+            }
+            beginLevel = tmpLevel;
+            tmpLevel.clear();
+            result += 1;
+        }
+        return 0;
+    }
 private:
-    void oneLetterDistance(string word, unordered_set<string>& hashSet, 
+    bool oneLetterDistance(string word, unordered_set<string>&wordSet, 
+                           unordered_set<string>& nextLevel, unordered_set<string>& visited,
+                           unordered_set<string>& endLevel) {
+        int wordLen = word.length();
+        string tmpWord = word;
+        for (int i = 0; i < wordLen; i++) {
+            char oriCh = word[i];
+            for (int c = 'a'; c <= 'z'; c++) {
+                if (c == oriCh) {
+                    continue;
+                }
+                tmpWord[i] = c;
+                if (endLevel.find(tmpWord) != endLevel.end()) {
+                    return true;
+                }
+                if (wordSet.find(tmpWord) != wordSet.end() && visited.find(tmpWord) == visited.end()) {
+                    nextLevel.insert(tmpWord);
+                }
+            }
+            tmpWord[i] = oriCh;
+        }
+        return false;
+    }
+
+    void oneLetterDistance(string word, unordered_set<string>& wordSet, 
                            queue<string>& level, unordered_set<string>& visited)
     {
         int wordLen = word.length();
@@ -62,7 +125,7 @@ private:
                     continue;  // avoid same string
                 }
                 tmpWord[i] = c;
-                if (hashSet.find(tmpWord) != hashSet.end() && visited.find(tmpWord) == visited.end()) {
+                if (wordSet.find(tmpWord) != wordSet.end() && visited.find(tmpWord) == visited.end()) {
                     level.push(tmpWord);
                 }
             }
@@ -81,12 +144,14 @@ int main() {
     endWord = "cog";
     wordList = {"hot", "dot", "dog", "lot", "log", "cog"};
     cout << "Min ladder length: " << obj.ladderLength(beginWord, endWord, wordList) << endl;
+    cout << "Min ladder length: " << obj.ladderLength2(beginWord, endWord, wordList) << endl;
 
     // case 2
     beginWord = "hit";
     endWord = "cog";
     wordList = {"hot", "dot", "dog", "lot", "log"};
     cout << "Min ladder length: " << obj.ladderLength(beginWord, endWord, wordList) << endl;
+    cout << "Min ladder length: " << obj.ladderLength2(beginWord, endWord, wordList) << endl;
 
     return 0;
 }
